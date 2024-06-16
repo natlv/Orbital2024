e83b2a7189993bbac906ade06f36c8b5dfebb3f9
GrassAppreciator <achong2014@gmail.com>
2024-06-17 00:53:13 +0800

    new user profile with point system

--- project/myapp/models.py
+++ project/myapp/models.py
@@ -28,3 +28,11 @@ class Event(models.Model):
     
     def __str__(self):
         return self.event_name
+    
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    profile_pic = models.ImageField(upload_to='myapp/files/profile_pics', blank=True)
+    points = models.IntegerField(default=20)
