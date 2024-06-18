e06cb5b78590c13d8d3a72a3bf3f14050ed35133
nathan <nathanlooenping@gmail.com>
2024-06-17 20:59:49 -0500

    Created template for marketplace and rewards. Streamlined profile view.

--- project/myapp/urls.py
+++ project/myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
-from .views import EventCreateView, EventJoinView, ChosenEventJoinView, MyEventsView, profile, edit_profile
+from .views import EventCreateView, EventJoinView, ChosenEventJoinView, MyEventsView, MarketplaceView, RewardsView, profile, edit_profile
 
 urlpatterns = [
     path('', views.index, name='home'),
@@ -13,6 +13,8 @@ urlpatterns = [
     path('event_join/', EventJoinView.as_view(), name='event_join'),
     path('event_join/<str:event_id>/', ChosenEventJoinView.as_view(), name='event_join_chosen'),
     path('my_events/', MyEventsView.as_view(), name='my_events'),
+    path('marketplace/', MarketplaceView.as_view(), name='marketplace'),
+    path('rewards/', RewardsView.as_view(), name='rewards'),
     path('profile/', profile, name='profile'),
     path('edit_profile/', edit_profile, name='edit_profile'),
 ]
