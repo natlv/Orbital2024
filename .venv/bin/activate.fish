be7d8925771b75b7077406fd3541a9ef85393355
nathan <nathanlooenping@gmail.com>
2024-06-21 14:01:48 +0800

    edited rewards database

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/nathan/Documents/orbital/Orbital2024-2/.venv"
+set -gx VIRTUAL_ENV "/Users/nathan/Orbital2024/Orbital2024/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
